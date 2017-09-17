c0ebaf09fe66be91bb926bae2636292475b82a83
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-09-16 21:22:09 -0500

    Add texas plot to main page of website

--- visualizer/website/views.py
+++ visualizer/website/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'website/index.html')
-
-def visualization(request):
     return render(request, 'website/texas.html')
